6548cecbc2d4bd854733e4c1c13d356aef56f215
krissasantos <krissa.santos31@gmail.com>
2016-11-05 17:05:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const app = "I don't do much."
+// const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+    function addBump(){
+      counter ++
+      
+    }
+    function getBumps(){
+        return counter
+        }
+    return {
+      addBump,
+      getBumps 
+      //when returning more than 1 function
+    }
+        
+}
+
+
+
+
+function createAnimal(animalType) {
+  return function (deadlyDevice){
+    // object = {animalType, deadlyDevice};
+    //   return object
+     return {animalType, deadlyDevice};
+ 
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
